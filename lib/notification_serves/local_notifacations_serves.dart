8002c925a4ab7784b61b475d136b4ab1b7f0ab1c
Esmail <semoa5488@gmail.com>
2024-06-27 11:40:43 -0700

    Add repeated notification and cansel notification

--- lib/notification_serves/local_notifacations_serves.dart
+++ lib/notification_serves/local_notifacations_serves.dart
@@ -11,7 +11,9 @@ class LocalNotifacationsServes {
 
   static Future init() async {
     InitializationSettings settings = const InitializationSettings(
-      android: AndroidInitializationSettings('@mipmap/ic_launcher'),
+       android: AndroidInitializationSettings('@mipmap/ic_launcher'),
+      //  android: AndroidInitializationSettings('appicon'),
+      //C:\Users\Esmail\StudioProjects\local_notifications\android\app\src\main\res\drawable\app_icon.png
       iOS: DarwinInitializationSettings(),
     );
     flutterLocalNotificationsPlugin.initialize(
@@ -21,11 +23,32 @@ class LocalNotifacationsServes {
     );
   }
 
-  static void showBasicNotification({ required String title , required String body}) async {
+  static void showBasicNotification(
+      {required String title, required String body}) async {
     NotificationDetails? details = const NotificationDetails(
-      android: AndroidNotificationDetails('id 1', 'name 1', importance: Importance.max , priority: Priority.high),
+      android: AndroidNotificationDetails('id 1', 'name 1',
+          importance: Importance.max, priority: Priority.high, playSound: true),
       iOS: DarwinNotificationDetails(),
     );
     await flutterLocalNotificationsPlugin.show(0, title, body, details);
+  }
+
+  static void showRepeatedNotification(
+      {
+      required String title, required String body,}) async{
+    NotificationDetails? details = const NotificationDetails(
+        android: AndroidNotificationDetails('id 2', 'name 2' , importance: Importance.max , priority: Priority.high),
+        iOS: DarwinNotificationDetails());
+    await flutterLocalNotificationsPlugin.periodicallyShow(
+      2,
+      title,
+      body,
+      RepeatInterval.everyMinute,
+      details,
+    );
+  }
+
+  static void canselNotification()async{
+    await flutterLocalNotificationsPlugin.cancel(2);
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -13,24 +13,33 @@ class HomeScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            myButton(title: 'Basic Notification', onTap: (){
-              LocalNotifacationsServes.showBasicNotification( title: 'الذهاب الى الجم' ,  body: ' تمرينه صدر و رجل وقفا و قرف و يلا بينا نخربها'  );
+            myButton(title: 'Basic Notification', isCansel: false ,onTap: (){
+              LocalNotifacationsServes.showBasicNotification( title: 'Basic Notification' ,  body: 'I need to go to gym'  );
+            }),
+            SizedBox(height: 10,),
+            myButton(title: 'Repeated Notification',isCansel: false,  onTap: (){
+              LocalNotifacationsServes.showRepeatedNotification( title: 'Repeated Notification' ,  body: 'I need to go to gym'  );
+            }),
+            SizedBox(height: 30,),
+            myButton(title: 'Cansel Notification', isCansel: true, onTap: (){
+              LocalNotifacationsServes.canselNotification();
             }),
           ],
         ),
       ),
     );
   }
-
+// C:\Users\Esmail\StudioProjects\local_notifications\android\app\src\main\res\drawable\appicon.png
   Widget myButton({
     required String title,
     required void Function()? onTap,
+    required bool isCansel,
 })=>GestureDetector(
     onTap: onTap ,
     child: Container(
       padding: EdgeInsets.all(12),
       decoration: BoxDecoration(
-        color: Colors.blue,
+        color: isCansel ?Colors.red :Colors.blue,
         borderRadius: BorderRadius.circular(10),
       ),
       child: Center(child: Text(title , style: TextStyle(color: Colors.white, fontSize: 18 ),)),
